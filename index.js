aaa775b5d4eb95881c78a338e24d8a4d2978813c
s-k3nyyy <collo.c.heg@gmail.com>
2024-04-10 14:13:05 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+ .then(response => response.json())
+.then(books => renderBooks(books))
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +12,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
